bd3e703e9e968cbddb24fb0223a9460b22027392
awiatros <90422853+awiatros@users.noreply.github.com>
2022-02-10 17:32:53 -0600

    Fixed animations and text labels

--- js/index.js
+++ js/index.js
@@ -1,3 +1,8 @@
 $(".burger").click(function(){
    $(".ham-nav").toggleClass("out");
 });
+
+$(".moveup").click(function() {
+   $('html, body').animate({
+       scrollTop: $(".navigation").offset().top
+   }, 100)});
