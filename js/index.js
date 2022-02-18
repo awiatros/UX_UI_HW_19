13b8eb391769ce1305f7869f6311e88f424cd272
awiatros <90422853+awiatros@users.noreply.github.com>
2022-02-18 14:38:44 -0600

    fixed link to case study

--- js/index.js
+++ js/index.js
@@ -6,7 +6,3 @@ $(".moveup").click(function() {
    $('html, body').animate({
        scrollTop: $("html").offset().top
    }, 100)});
-
-   $('.lsp_btn').click(function() {
-      window.location = "https://awiatros.github.io/UX_UI_HW_20/" + this.id;
-   });
